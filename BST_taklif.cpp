3535464a51c6849e9999ba1b3925ad651c9fb999
Purya-s <156438302+Purya-s@users.noreply.github.com>
2024-01-13 11:06:00 +0330

    Add files via upload

---
+++ BST_taklif.cpp
@@ -0,0 +1,132 @@
+
+
+#include <iostream>
+
+
+
+using namespace std;
+
+class Node 
+{
+private:
+    int key;
+    Node* left;
+    Node* right;
+
+public:
+    Node(int key) {
+        this->key = key;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    int getKey()  {
+        return key;
+    }
+
+    Node* getLeft()  {
+        return left;
+    }
+
+    void setLeft(Node* left) {
+        Node::left = left;
+    }
+
+    Node* getRight()  {
+        return right;
+    }
+
+    void setRight(Node* right) {
+        Node::right = right;
+    }
+
+};
+
+
+Node* newNode(int key) {
+    return new Node(key);
+}
+
+
+Node* insertRec(Node* root, int key) {
+    if (root == nullptr) {
+        return newNode(key);
+    }
+
+    if (key < root->getKey()) {
+        root->setLeft(insertRec(root->getLeft(), key));
+    }
+    else if (key > root->getKey()) {
+        root->setRight(insertRec(root->getRight(), key));
+    }
+
+    return root;
+}
+
+Node* insert(Node* root, int key) {
+    return insertRec(root, key);
+}
+
+bool search(Node* root, int key) {
+    if (root == nullptr || root->getKey() == key) {
+        return root != nullptr;
+    }
+
+    if (key > root->getKey()) {
+        return search(root->getRight(), key);
+    }
+
+    return search(root->getLeft(), key);
+}
+
+void inorderTraversal(Node* root) {
+    if (root != nullptr) {
+        inorderTraversal(root->getLeft());
+        std::cout << root->getKey() << " ";
+        inorderTraversal(root->getRight());
+    }
+}
+
+
+int main() {
+    Node* root = nullptr;
+
+    while (true) {
+
+
+        cout << "Enter a number : \n"
+            << "1. Add to tree \n"
+            << "2. Search in tree \n"
+            << "3. Exit \n";
+
+        char selected;
+        cin >> selected;
+
+        int key;
+        switch (selected) {
+        case '1':
+
+            cout << "Enter the Key: ";
+            cin >> key;
+            root = insert(root, key);
+            break;
+
+        case '2':
+            cout << "Enter the Key: ";
+            cin >> key;
+            if (search(root, key)) {
+                cout << key << " is found" << endl;
+            }
+            else {
+                cout << key << " is not found" << endl;
+            }
+            break;
+
+        case '3':
+            exit(0);
+
+        default:
+            cout << "wrong Number...";
+        }
+    }
+}
